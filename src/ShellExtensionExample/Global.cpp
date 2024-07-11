50d00a96610202e8143d3a77e2e6db95a7201ff5
yqh <haihoing@qq.com>
2024-07-11 16:04:21 +0800

    const CLSID CLSID_OverlayIconExt = // 图标叠加

--- src/ShellExtensionExample/Global.cpp
+++ src/ShellExtensionExample/Global.cpp
@@ -4,12 +4,13 @@
 #include "MenuExt.h"
 
 
-const CLSID CLSID_MenuExt = 
-{ 0x95fb130, 0x783f, 0x43d7, { 0xb4, 0xd5, 0xd2, 0x7d, 0xcf, 0xf3, 0x83, 0xc4 } };
+const CLSID CLSID_MenuExt = // ÓÒ¼ü²Ëµ¥¡£
+{ 0x95fb130, 0x783f, 0x43d7, { 0xb4, 0xd5, 0xd2, 0x7d, 0xcf, 0xf3, 0x83, 0xc3 } };
+const wchar_t* FRIENDLY_NAME_MenuExt = L"FastPdfContextMenu";
 
-const CLSID CLSID_OverlayIconExt = 
-{ 0x461b9859, 0x6952, 0x47ed, { 0x93, 0x63, 0x69, 0x86, 0xda, 0xec, 0x39, 0xec } };
-
+const CLSID CLSID_OverlayIconExt = // Í¼±êµþ¼Ó
+{ 0x461b9859, 0x6952, 0x47ed, { 0x93, 0x63, 0x69, 0x86, 0xda, 0xec, 0x39, 0xeb } };
+const wchar_t* FRIENDLY_NAME_OverlayIconExt = L"FastPdfOverlayIcon";
 
 Global::Global(): m_hInst(nullptr), m_cDllRef(0)
 {
@@ -26,14 +27,14 @@ void Global::InitializeExtensionsData()
     m_FactoryData.push_back(
         FactoryInfo(CLSID_OverlayIconExt, 
         &example::OverlayIconExt::ComponentCreator,
-        L"ExampleOverlayIcon"
+            FRIENDLY_NAME_OverlayIconExt
         )
     );
 
     m_FactoryData.push_back(
         FactoryInfo(CLSID_MenuExt, 
         &example::MenuExt::ComponentCreator,
-        L"ExampleContextMenu"
+            FRIENDLY_NAME_MenuExt
         )
     );
 }
